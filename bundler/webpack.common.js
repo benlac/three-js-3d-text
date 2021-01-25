f9beaf9b0ca636408780731e37f390cfe42e3310
benlac <benoit.lacombled@gmail.com>
2021-01-25 23:04:18 +0100

    Install sass and configure the default canvas style

--- bundler/webpack.common.js
+++ bundler/webpack.common.js
@@ -54,6 +54,19 @@ module.exports = {
                 ]
             },
 
+            // SCSS
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                  // Creates `style` nodes from JS strings
+                  "style-loader",
+                  // Translates CSS into CommonJS
+                  "css-loader",
+                  // Compiles Sass to CSS
+                  "sass-loader",
+                ],
+              },
+
             // Images
             {
                 test: /\.(jpg|png|gif|svg)$/,
--- src/script.js
+++ src/script.js
@@ -1,2 +1,2 @@
-import './style.css';
+import './style.scss';
 import * as THREE from 'three';
